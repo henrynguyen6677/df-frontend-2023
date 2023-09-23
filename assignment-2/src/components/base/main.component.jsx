babb0702753db4b2c953dae020d7ad207814f827
henrynguyen6677 <henrynguyen6677@gmail.com>
2023-09-24 01:12:20 +0700

    [assignment-2] Add main component

---
+++ assignment-2/src/components/base/main.component.jsx
@@ -0,0 +1,8 @@
+import {ID_NAMES} from "../../contants/ids.constant";
+import {CLASS_NAMES} from "../../contants/classes.constant";
+
+export default function MainComponent () {
+  return <div id={ID_NAMES.main} className={CLASS_NAMES.backgroundGray}>
+
+  </div>
+}
