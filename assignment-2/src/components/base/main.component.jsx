761c42bb076675713047d6ace98f2a0a94c6885f
henrynguyen6677 <henrynguyen6677@gmail.com>
2023-10-01 10:53:46 +0700

    [assignment-2] Refactor search to support case-sensitive

--- assignment-2/src/components/base/main.component.jsx
+++ assignment-2/src/components/base/main.component.jsx
@@ -1,21 +1,22 @@
-import {ID_NAMES} from "../../contants/ids.constant";
+import { ID_NAMES } from "../../contants/ids.constant";
 import TableComponent from "../table/table.component";
 import SearchComponent from "../search/search.component";
-import {LocalStorage} from "../../utils/localstore";
-import {BOOKS} from "../../contants/storage";
+import { LocalStorage } from "../../utils/localstore";
+import { BOOKS } from "../../contants/storage";
 import PagingComponent from "../paging/paging.component";
 import ProfileComponent from "../profile/profile.component";
 
-export default function MainComponent () {
+export default function MainComponent() {
   return (
     <div id={ID_NAMES.main}>
       <div id="container">
         <ProfileComponent />
-        <SearchComponent defaultRows={JSON.parse(LocalStorage.getItem(BOOKS))}/>
-        <TableComponent/>
-        <PagingComponent/>
+        <SearchComponent
+          defaultBooks={JSON.parse(LocalStorage.getItem(BOOKS))}
+        />
+        <TableComponent />
+        <PagingComponent />
       </div>
-
     </div>
-  )
+  );
 }
--- assignment-2/src/components/search/search.component.jsx
+++ assignment-2/src/components/search/search.component.jsx
@@ -1,35 +1,39 @@
-import {useContext} from "react";
-import {OverlayContext} from "../../contexts/overlay.context";
+import { useContext } from "react";
+import { OverlayContext } from "../../contexts/overlay.context";
 import PropTypes from "prop-types";
-import {BookRowsContext} from "../../contexts/book-rows.context";
+import { BookRowsContext } from "../../contexts/book-rows.context";
+import { SearchHelper } from "../../utils/search.helper";
 
-export default function SearchComponent ({ defaultRows }) {
-  const bookRowsContext = useContext(BookRowsContext)
-  const { setRows, rows } = bookRowsContext
+export default function SearchComponent({ defaultBooks }) {
+  const bookRowsContext = useContext(BookRowsContext);
+  const { setBooks, books } = bookRowsContext;
   const handleFilter = (e) => {
-    const value = e.target.value
-    if (value === '' && rows.length === 0) {
-      setRows(defaultRows)
+    const filter = e.target.value;
+    if (filter === "" && books.length === 0) {
+      setBooks(defaultBooks);
       return;
     }
-    const newRows = defaultRows?.filter((item) =>
-      item.name.includes(value) ||
-      item.author.includes(value) ||
-      item.topic.includes(value)).map(item => item)
-    setRows(newRows)
-  }
+    const newBooks = SearchHelper(defaultBooks, filter);
+    setBooks(newBooks);
+  };
 
-  const overlayContext = useContext(OverlayContext)
-  const handleAdd = () => overlayContext.showAddOverlay(true)
+  const overlayContext = useContext(OverlayContext);
+  const handleAdd = () => overlayContext.showAddOverlay(true);
 
   return (
     <div id="search">
-      <input id="searchInput" onChange={handleFilter} placeholder="Search books"/>
-      <button onClick={handleAdd} id="addBook">Add book</button>
+      <input
+        id="searchInput"
+        onChange={handleFilter}
+        placeholder="Search books"
+      />
+      <button onClick={handleAdd} id="addBook">
+        Add book
+      </button>
     </div>
-  )
+  );
 }
 
 SearchComponent.propsType = {
   defaultRows: PropTypes.array,
-}
+};
